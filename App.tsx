30fcaa6cc8e78e974d698ed0fad0c275c155472f
demilade111 <demiladealuko111@gmail.com>
2023-05-02 10:14:25 +0100

    updated file

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Text, View } from "react-native";
+import styled from "styled-components";
 
 export default function App() {
+  const [stop, setStop] = useState<Boolean>(false);
+  const Container = styled(View)`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+    background-color: #fff;
+  `;
+
   return (
-    <View style={styles.container}>
+    <Container>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
