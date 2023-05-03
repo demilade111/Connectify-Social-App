5f62edf6dda6ca36460070a33d1f0996ceb0e0fb
demilade111 <demiladealuko111@gmail.com>
2023-05-03 06:38:09 +0100

    added logo and also added font to the App

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import * as Font from "expo-font";
+
 import styled from "styled-components";
+import { Text, View } from "react-native";
+
+const loadFonts = async () => {
+  await Font.loadAsync({
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-ExtraBoldItalic.ttf"),
+  });
+};
+const Container = styled(View)`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+`;
+const Paragraph = styled(Text)`
+  font-family: Poppins-Bold;
+  color:red;
+`;
 
 export default function App() {
-  const [stop, setStop] = useState<Boolean>(false);
-  const Container = styled(View)`
-    flex: 1;
-    justify-content: center;
-    align-items: center;
-    background-color: #fff;
-  `;
+  const [isReady, setIsReady] = useState(false);
+
+  useEffect(() => {
+    loadFonts().then(() => setIsReady(true));
+  }, []);
+
+  if (!isReady) {
+    return null;
+  }
 
   return (
     <Container>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Paragraph>demihj</Paragraph>
     </Container>
   );
 }
