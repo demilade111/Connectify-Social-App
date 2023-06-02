9c712260a3dd5cbf59861334d21f58b23e148359
Moyosore Olumide <moyosoreolumideobi@gmail.com>
2023-06-02 12:19:30 +0100

    design: added "add post" screen

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,7 @@
 import { StyleSheet, Text } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import React from "react";
-import { Wrapper } from "./styles";
+import { CenterText, Wrapper } from "./styles";
 import CustomIcon from "../../shared/Icon";
 import { HeaderPropsType } from "../../types";
 
@@ -10,7 +10,7 @@ const HeaderContent = (props: HeaderPropsType) => {
   return (
     <Wrapper insets={insets}>
       <CustomIcon name="chevron-back-sharp" type="ionicon" />
-      <Text>{props?.title}</Text>
+      <CenterText>{props?.title}</CenterText>
       <Text></Text>
     </Wrapper>
   );
--- src/components/Post/AddPost/index.tsx
+++ src/components/Post/AddPost/index.tsx
@@ -1,13 +1,29 @@
 import { StyleSheet, Text } from "react-native";
 import React from "react";
 import HeaderContent from "../../Header";
+import {
+  AddCaptionTextArea,
+  AddFormLabelText,
+  AddFormView,
+  AddHastagsTextInput,
+  UploadButton,
+  UploadButtonText,
+} from "./styles";
 
 const AddPostContent = () => {
   return (
     <>
       <HeaderContent title="Post" />
+      <AddFormView>
+        <AddFormLabelText>Add caption</AddFormLabelText>
+        <AddCaptionTextArea />
+        <AddFormLabelText>Add Hashtags</AddFormLabelText>
+        <AddHastagsTextInput />
 
-      <Text>AddPostContent</Text>
+        <UploadButton>
+          <UploadButtonText>Upload</UploadButtonText>
+        </UploadButton>
+      </AddFormView>
     </>
   );
 };
